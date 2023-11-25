685943dc3ea0d64726f1685ad2db86f80355dc2f
WigglyDon <donfather1000@gmail.com>
2023-11-25 11:26:42 -0700

    update correct solution using a Set

--- main.test.js
+++ main.test.js
@@ -3,5 +3,5 @@ const { generateRandomSet } = require('./main');
 test('Generated set has 10,000 unique numbers', () => {
   const generatedSet = generateRandomSet();
   expect(generatedSet).toHaveLength(10000);
-  expect(new Set(generatedSet)).toHaveLength(10000);
+  expect(new Set(generatedSet).size).toBe(10000);
 });
